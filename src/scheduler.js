09004a67ee2662c141ae9feab3a8d13979c51837
vannguyenv12 <npnv.vn1@gmail.com>
2025-08-04 11:24:23 +0700

    update cron job

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,10 +1,24 @@
 const cron = require('node-cron');
-const { exec } = require('child_process');
 const checkUSCISUpdates = require('./checkStatusScheduler');
 
 console.log('Chạy định kỳ');
 
-cron.schedule('*/30 * * * *', () => {
-  console.log('⏰ Đang chạy kiểm tra định kỳ USCIS...');
-  checkUSCISUpdates();
+let isRunning = false;
+cron.schedule('*/30 * * * *', async () => {
+  if (isRunning) {
+    console.log('⚠️ Đang có phiên USCIS đang chạy → bỏ qua lần gọi này');
+    return;
+  }
+
+  isRunning = true;
+  console.log('⏰ Bắt đầu phiên USCIS');
+
+  try {
+    await checkUSCISUpdates();
+    console.log('✅ Hoàn tất phiên USCIS');
+  } catch (err) {
+    console.error('💥 Lỗi trong USCIS:', err.message);
+  } finally {
+    isRunning = false;
+  }
 });
